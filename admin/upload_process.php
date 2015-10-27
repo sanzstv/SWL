1b49e6a33b436d2b1f24f73e1bbce565c7dbcd52
tchen <tchenGIT@users.noreply.github.com>
2015-10-26 17:00:03 -0700

    reinstated HTAccess

--- admin/upload_process.php
+++ admin/upload_process.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html>
 <body>
+  <button><a href='index.html'>Return</a></button>
+  <br />
   <?php
-    $directory = "../upload/";
+    $directory = "../resource/";
     $file_target = $directory . basename($_FILES["courseList"]["name"]);
     $file_extension = pathinfo($file_target, PATHINFO_EXTENSION);
     if( strcasecmp( $file_extension, 'csv' ) == 0 ) {
       if(! move_uploaded_file($_FILES["courseList"]["tmp_name"], $file_target))
         echo "Upload Failure <br />";
-      else
+      else {
         echo "Upload Success <br />";
+      }
     } else
       echo "Incorrect File Format! Please Try Again.";
   ?>
-  <button><a href='index.html'>Return</a></button>
 </body>
 </html>
 
