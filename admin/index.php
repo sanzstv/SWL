c61d290d061c95abb1d93d379b7e581157c9bace
tchen <tchenGIT@users.noreply.github.com>
2015-10-29 11:53:41 -0700

    updated ui and made admin index into php page

---
+++ admin/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>SCUWaitLister Admin Page</h1>
+    <p>You are using this system as <?php echo $_SERVER['REMOTE_USER']; ?>.</p>
+    <button><a href='upload.html'>Update Course</a></button>
+    <button><a href='view.html'>View Requests</a></button>
+  </body>
+</html>
