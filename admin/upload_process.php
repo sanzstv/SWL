aa77af712819e681fc818f6cd08e10e6da905688
tchen <tchenGIT@users.noreply.github.com>
2015-10-29 11:42:33 -0700

    added error checking in uploading of course files

--- admin/upload_process.php
+++ admin/upload_process.php
@@ -5,16 +5,41 @@
   <br />
   <?php
     $directory = "../resource/";
-    $file_target = $directory . basename($_FILES["courseList"]["name"]);
-    $file_extension = pathinfo($file_target, PATHINFO_EXTENSION);
-    if( strcasecmp( $file_extension, 'csv' ) == 0 ) {
-      if(! move_uploaded_file($_FILES["courseList"]["tmp_name"], $file_target))
-        echo "Upload Failure <br />";
-      else {
-        echo "Upload Success <br />";
-      }
-    } else
-      echo "Incorrect File Format! Please Try Again.";
+    $fileTarget = $directory . basename($_FILES["courseList"]["name"]);
+    $fileExt = pathinfo($fileTarget, PATHINFO_EXTENSION);
+    if( strcasecmp($fileExt, 'csv') != 0 )
+      die("Incorrect file extension! Please use CSV files.");
+    $tmpFile = $_FILES["courseList"]["tmp_name"];
+
+    $fp = fopen($tmpFile, "r");
+    $fw = fopen($fileTarget, "w");
+    $lineNumber = 0;
+    while( ($data = fgetcsv($fp)) !== false ) {
+      $lineNumber++;
+      if( count($data) == 1 && $data[0] == null )
+        continue;
+      if( count($data) != 3 )
+        die("Incorrect number of fields on line $lineNumber.");
+      $courseSection = trim($data[0]);
+      $courseNumber = trim($data[1]);
+      $courseName = trim($data[2]);
+      if( strlen($courseSection) != 5 || ! is_numeric($courseSection) )
+        die(
+          "Course Section on line $lineNumber is not in correct format.<br />" .
+          "$lineNumber : $courseSection, $courseNumber, $courseName"
+        );
+      if( strlen($courseNumber) != 3 || ! is_numeric($courseNumber) )
+        die(
+          "Course Number on line $lineNumber is not in correct format.<br />" .
+          "$lineNumber : $courseSection, $courseNumber, $courseName"
+        );
+      if( ! fwrite($fw, "$courseSection,$courseNumber,$courseName\n") )
+        die("Error writing file to server. Please contact the administrator.");
+    }
+    fclose($fw);
+    fclose($fp);
+
+    echo "Upload Success <br />";
   ?>
 </body>
 </html>
