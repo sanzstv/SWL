784e23c72511ed7761292c0fa395db62844403e7
tchen <tchenGIT@users.noreply.github.com>
2015-10-29 14:40:17 -0700

    added all department directories

--- admin/index.php
+++ admin/index.php
@@ -4,6 +4,6 @@
     <h1>SCUWaitLister Admin Page</h1>
     <p>You are using this system as <?php echo $_SERVER['REMOTE_USER']; ?>.</p>
     <a href='upload.html'><button>Update Course</button></a>
-    <a href='view.html'><button>View Requests</button></a>
+    <a href='view.php'><button>View Requests</button></a>
   </body>
 </html>
