d7d9fc8cbfa8672ed065247436b2e945cb69c230
gregcusack <gregorycusack@gmail.com>
2015-10-30 11:16:30 -0700

    changed format of selecting waitlist
    still need to fix it so it links to the correct waitlist

--- admin/selectWaitlist.php
+++ admin/selectWaitlist.php
@@ -1,24 +1,52 @@
 <!DOCTYPE html>
 <html>
-  <body>
-    <h1>SCUWaitLister Admin Page</h1>
-    <p>You are using this system as <?php echo $_SERVER['REMOTE_USER']; ?>.</p>
-    <p>Select a waitlist to view:<p>
-    <?php
-    $row=1;
-    ini_set('auto_detect_line_endings', TRUE);
-    if(($handle = fopen("../resource/COEN/courseList.csv", "r")) !== FALSE) {
-	while(($data= fgetcsv($handle, 1000, ",")) !== FALSE) {
-	    $num = count($data);
-	    for($c=0; $c<$num; $c+=3) {
-		echo "<p>";
-		echo "<a href='parseCSV.php'><button>$data[1]:$data[2] (Section: $data[0])</button></a>";
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 5px;
+}
+</style>
+</head>
+
+<body>
+  <h1>SCUWaitLister Admin Page</h1>
+  <p>You are using this system as <?php echo $_SERVER['REMOTE_USER']; ?>.</p>
+  <p>Select a waitlist to view:<p>
+<table style="width:80%">
+  <tr>
+    <th>See waitlist</th>
+    <th>Section number</th>
+    <th>Course number</th>
+    <th>Course name</th>
+  </th>
+<?php
+$row=1;
+ini_set('auto_detect_line_endings', TRUE);
+if(($handle = fopen("courseList.csv", "r")) !== FALSE) {
+    while(($data= fgetcsv($handle, 1000, ",")) !== FALSE) {
+	$num = count($data);
+	for($c=0; $c<$num; $c+=3) {
+	    echo "<p>";
+	    echo "<tr>"; 
+	    echo "<td>"; 
+	    echo "<a href='parseCSV.php'><button>Select</button></a>";
+	    foreach($data as $cell) {
+		echo "<td>" . htmlspecialchars($cell);
 	    }
-	}
-        fclose($handle);
+	    echo"</tr>\n";
+	    //echo "<a href='parseCSV.php'><button>$data[1]:$data[2] (Section: $data[0])</button></a>";
+	    }
     }
-    ini_set('auto_detect_line_endings', FALSE);
-    ?> 
-        
-  </body>
+    fclose($handle);
+}
+ini_set('auto_detect_line_endings', FALSE);
+?> 
+
+</table>	
+</body>
 </html>
+
