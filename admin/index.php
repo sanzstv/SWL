346f0858cd099870f11eacc28490726b686ba565
tchen <tchenGIT@users.noreply.github.com>
2015-10-31 15:48:02 -0700

    refactor selectwaitlist.php to select_waitlist.php

--- admin/index.php
+++ admin/index.php
@@ -4,6 +4,6 @@
     <h1>SCUWaitLister Admin Page</h1>
     <p>You are using this system as <?php echo $_SERVER['REMOTE_USER']; ?>.</p>
     <a href='upload.php'><button>Update Course</button></a>
-    <a href='selectWaitlist.php'><button>View Requests</button></a>
+    <a href='select_waitlist.php'><button>View Requests</button></a>
   </body>
 </html>
