0bb6e709768ed6cc43728ae8d129cb19e25af07f
gregcusack <gregorycusack@gmail.com>
2015-11-01 15:03:29 -0800

    added grad year and changed order

--- student/waitlist.php
+++ student/waitlist.php
@@ -11,10 +11,11 @@
 
 <h4 class = "des">Student Waitlist Request</h4>
 <form action = "request.php" method = "POST">
-        First Name:<input name = "first" label = "First Name" type = "text" maxlength = "30" id ="fname" required = "required"\> </br>
-        Last Name:<input name = "last" label = "Last Name" type = "text" maxlength = "40" id ="lname" required = "required"\></br>
+        Last Name:<input name = "first" label = "Last Name" type = "text" maxlength = "40" id ="fname" required = "required"\> </br>
+        First Name:<input name = "last" label = "First Name" type = "text" maxlength = "30" id ="lname" required = "required"\></br>
+        Student ID Number:<input name = "studentid" label = "Student ID Number" type = "text" maxlength = "11" id = "studentid" value = "0000163542" required = "required"\></br>
+        Graduation Year:<input name = "gradYear" label = "Graduation Year" type = "text" maxlength = "4" id = "gradYear" required = "required"\n></br>
         SCU Email Login:<input name = "email" label = "SCU Email Login" type = "text" maxlength = "43" id ="email" value="ex:jdoe" required = "required"\>@scu.edu</br>
-        Student ID Number:<input name = "studentid" label = "Student ID Number" type = "text" maxlength = "11" id ="studentid" value="00000163542" required = "required"\></br>
         Reason for requiring enrollment (max. 500 characters)</br>
         <textarea id = "reason" cols = "50" rows = "10" required = "required" value = "Please explain why you need to be enrolled in this class."></textarea></br>
         <div class="g-recaptcha" data-sitekey="6LdJGBATAAAAADdwl_wupyRNeiy1go0ZUk2bEu1P" required = "required"></div> 
