7832b631d0ec9b01ad2ae616fb545bd795ff97d5
tchen <tchenGIT@users.noreply.github.com>
2015-10-31 18:59:34 -0700

    added return button to all pages

--- admin/upload.php
+++ admin/upload.php
@@ -3,8 +3,9 @@
   <body>
     <h1>SCUWaitLister Update Course Page</h1>
     <p>You are using this system as <?php echo $_SERVER['REMOTE_USER']; ?>.</p>
+    <a href='index.php'><button>Return</button></a><br />
+    <p>Upload new course list for your department in CSV format. <br /></p>
     <form action='upload_process.php' method='post' enctype='multipart/form-data'>
-      Upload new course list for your department in csv format. <br /><br />
       <input type='file' name='courseList' id='courseList' />
       <input type='submit' value='Upload' />
     </form>
