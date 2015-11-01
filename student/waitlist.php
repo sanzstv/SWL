f4e8f0fd79ced9fa57b525122d6c6fd625c267a1
gregcusack <gregorycusack@gmail.com>
2015-11-01 14:47:04 -0800

    implemented max char input length
    also added recaptcha but currently, its not required to submit form so i am working on that now

--- student/waitlist.php
+++ student/waitlist.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang = "en">
+<html>
 <head>
         <title>Students | SCUWaitlister</title>
 
@@ -11,15 +11,15 @@
 
 <h4 class = "des">Student Waitlist Request</h4>
 <form action = "request.php" method = "POST">
-        First Name:<input name = "first" label = "First Name" type = "text" id ="fname" required = "required"\> </br>
-        Last Name:<input name = "last" label = "Last Name" type = "text" id ="lname" required = "required"\></br>
-        SCU Email Login:<input name = "email" label = "SCU Email Login" type = "text" id ="email" value="ex:jdoe" required = "required"\>@scu.edu</br>
-        Student ID Number:<input name = "studentid" label = "Student ID Number" type = "text" id ="studentid" value="00000163542" required = "required"\></br>
-Reason for requiring enrollment (max. 500 words)</br>
+        First Name:<input name = "first" label = "First Name" type = "text" maxlength = "30" id ="fname" required = "required"\> </br>
+        Last Name:<input name = "last" label = "Last Name" type = "text" maxlength = "40" id ="lname" required = "required"\></br>
+        SCU Email Login:<input name = "email" label = "SCU Email Login" type = "text" maxlength = "43" id ="email" value="ex:jdoe" required = "required"\>@scu.edu</br>
+        Student ID Number:<input name = "studentid" label = "Student ID Number" type = "text" maxlength = "11" id ="studentid" value="00000163542" required = "required"\></br>
+        Reason for requiring enrollment (max. 500 characters)</br>
         <textarea id = "reason" cols = "50" rows = "10" required = "required" value = "Please explain why you need to be enrolled in this class."></textarea></br>
+        <div class="g-recaptcha" data-sitekey="6LdJGBATAAAAADdwl_wupyRNeiy1go0ZUk2bEu1P" required = "required"></div> 
         <button type = "submit" value = "Submit">Submit Waitlist Request</button>
 
-
 </form>
-
+<script src='https://www.google.com/recaptcha/api.js'></script>
 </html>
