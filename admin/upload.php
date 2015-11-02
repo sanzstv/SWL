1b93c20dbf67036a9545b15cf4212e9960a165b3
gregcusack <gregorycusack@gmail.com>
2015-11-01 22:26:14 -0800

    Update upload.php
    provided example on how the .csv file should be formatted for uploading

--- admin/upload.php
+++ admin/upload.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
+  <style>
+    .example{color: #848484;}
+  </style>
   <body>
     <h1>SCUWaitLister Update Course Page</h1>
     <p>
@@ -7,7 +10,9 @@
       To change the Department, please restart the browser.
     </p>
     <a href='index.php'><button>Return</button></a><br />
-    <p>Upload new course list for your department in CSV format. <br /></p>
+    <p>Upload new course list for your department in CSV format: <br />
+    <span class = "example">sectionNumber, courseNumber, courseName<br />
+    i.e. 30991,174,Software Engineering</span></p>
     <form action='upload_process.php' method='post' enctype='multipart/form-data'>
       <input type='file' name='courseList' id='courseList' />
       <input type='submit' value='Upload' />
