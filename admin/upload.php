732f51f7aa8a94f04f2f4535854e3881716c6717
tchen <tchenGIT@users.noreply.github.com>
2015-11-02 09:25:43 -0800

    removed unnecessary stuff

--- admin/upload.php
+++ admin/upload.php
@@ -11,8 +11,6 @@
     </p>
     <a href='index.php'><button>Return</button></a><br />
     <p>Upload new course list for your department in CSV format: <br />
-    <span class = "example">sectionNumber,courseNumber,courseName<br />
-    i.e. 30991,174,Software Engineering</span></p>
     <form action='upload_process.php' method='post' enctype='multipart/form-data'>
       <input type='file' name='courseList' id='courseList' />
       <input type='submit' value='Upload' />
