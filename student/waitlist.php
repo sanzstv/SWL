160bc3edb210cafc2bcf0afbf559619c59055376
gregcusack <gregorycusack@gmail.com>
2015-11-01 17:21:49 -0800

    update

--- student/waitlist.php
+++ student/waitlist.php
@@ -2,8 +2,9 @@
 <html>
 <head>
 <style>
-        .error {color: #FF0000;}
-        <title>Students | SCUWaitlister</title>
+  .error {color: #FF0000;}
+  <title>Students | SCUWaitlister</title>
+  <script src = 'https://www.google.com/recaptcha/api.js'></script>
 </style>
 </head>
 
@@ -11,7 +12,7 @@
 
 <?php
 $lastErr = $firstErr = $studentIDErr = $gradYearErr = $emailErr = $reasonErr = "";
-$last = $first = $studentID = $gradYear = $email = $reason = "";
+$last = $first = $studentID = $gradYear = $email = $reason = $captcha = "";
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     if(empty($_POST["last"])) {
@@ -25,19 +26,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     else {
         $first = test_input($_POST["first"]);
-    }   
+    }
     if(empty($_POST["studentID"])) {
         $studentIDErr = "ID number required";
-    }   
+    }
     else {
         $studentID = test_input($_POST["studentID"]);
-    }   
+    }
     if(empty($_POST["gradYear"])) {
         $gradYear = "Graduation year required";
-    }   
+    }
     else {
         $gradYear = test_input($_POST["gradYear"]);
-    }   
+    }
     if(empty($_POST["email"])) {
         $email = "E-mail required";
     }   
@@ -50,8 +51,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     else {
         $reason = test_input($_POST["reason"]);
     }   
-}
+    if(isset($_POST['g-captcha-response'])) {
+        $captcha = $_POST['g-captcha-response'];
+    }   
+    if(!$captcha) {
+        echo '<h2>Please check the captcha box.</h2>';
+        exit;
+    }
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LdJGBATAAAAAKaI13d4CGW6J9Ad5GSXkvBuo0NO&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
 
+    if($response.success == false) {
+        echo '<h2> please die, you are spamming bruh</h2>';
+    }
+    else {
+        echo '<h2> You have been added to the waitlist</h2>';
+    }
+}
 function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
@@ -69,7 +84,7 @@ Last Name: <input name = "first" label = "Last Name" type = "text" maxlength = "
 First Name: <input name = "last" label = "First Name" type = "text" maxlength = "30"> <span class = "error">* <?php echo $firstErr;?></span><br><br>
 Student ID Number (i.e. 00000117281): <input name = "studentID" label = "Student ID Number" type = "text" pattern = "[0-9]{11}" maxlength = "11"> <span class = "error">*<?php echo $studentIDErr;?></span><br><br>
 Graduation Year (i.e. 2016): <input name = "gradYear" label = "Graduation Year" type = "text" pattern = "[0-9]{4}" maxlength = "4"> <span class = "error">*<?php echo $gradYearErr;?></span><br><br>
-SCU Email Login: <input name = "email" label = "SCU Email Login" type = "email" maxlength = "43"\> <span class = "error">*<?php echo $emailErr;?></span><br><br>
+SCU Email Login (i.e. jdoe@scu.edu): <input name = "email" label = "SCU Email Login" type = "email" pattern = "[a-zA-Z0-9]+@[a-zA-Z0-9.]+.[a-zA-Z]" maxlength = "43"\> <span class = "error">*<?php echo $emailErr;?></span><br><br>
         Reason for requiring enrollment (max. 500 characters):</br>
         <textarea id = "reason" cols = "50" rows = "10" required = "required" value = "Please explain why you need to be enrolled in this class."></textarea> <span class = "error">*<?php echo $reasonErr;?></span><br><br>
         <div class="g-recaptcha" data-sitekey="6LdJGBATAAAAADdwl_wupyRNeiy1go0ZUk2bEu1P" required = "required"></div>
