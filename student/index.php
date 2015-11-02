6abd98308937af4e2a6f40f6d4b91122e2f12588
tchen <tchenGIT@users.noreply.github.com>
2015-11-01 18:22:07 -0800

    changed look and feel

--- student/index.php
+++ student/index.php
@@ -1,42 +1,32 @@
 <!DOCTYPE html>
 <html lang = "en">
 <head>
-        <title>Students | SCUWaitlister</title>
-
+  <title>Students | SCUWaitlister</title>
 </head>
-
 <body>
+  <h1>SCUWaitLister Department Selection</h1>
 
-<h2 class = "header">SCUWaitlister</h2>
+  <a href='../index.html'><button>Return</button></a><br />
 
-<h4 class = "des">Select Department</h4>
+  <p>
+    Select the department of the class in which you wish to be waitlisted in.</br>
+    A list of classes is available at <a href="http://www.scu.edu/courseavail/" target="_blank">CourseAvail</a>.
+  </p>
 
+  <form action="selection.php" method = "post">
+    Department:
+    <select required id = "department" name = "department">
+      <option value = "">Select One</option>
+      <?php
+        foreach(glob("../resource/*") as $file) {
+          $dept = trim(basename($file).PHP_EOL);
+          echo "<option value='$dept'>$dept</option>";
+        } 
+      ?>
+    </select>
+    </br></br>
+    <button type = "submit" value = "Submit">Submit</button>
 
-<p>Select the department of the class waitlist you wish to enroll in.</br></br>
-
-A list of classes for the current quarter can be found at <a href="http://www.scu.edu/courseavail/" target="_blank">CourseAvail.</a></p>
-
-
-<form action="course.php" method = "post">
-        Department:
-        <select required id = "department" name = "department">
-                <option value = "">Select one</option>
-                 <?php
-                //list departments with available waitlists
-                 foreach(glob("../resource/*") as $file)
-                 {
-                        //parse file names
-                        $depts = trim(basename($file).PHP_EOL);
-                ?>
-                 <option value="<?= $depts ?>"><?= $depts ?></option>
-                <?php
-                } 
-                ?>
-        </select>
-        </br></br>
-        <button type = "submit" value = "Submit">Submit</button>
-
-</form>
+  </form>
 </body>
-
 </html>
