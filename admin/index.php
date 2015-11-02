4d9e17aafe63372db64c0dbd46aa17c9f3695fa0
tchen <tchenGIT@users.noreply.github.com>
2015-11-01 19:28:47 -0800

    updated how admin views waitlists

--- admin/index.php
+++ admin/index.php
@@ -6,6 +6,7 @@
       You are using this system as the <?php echo $_SERVER['REMOTE_USER']; ?> Department.<br />
       To change the Department, please restart the browser.
     </p>
+    <a href='../index.html'><button>Return</button></a>
     <a href='upload.php'><button>Update Course</button></a>
     <a href='select_waitlist.php'><button>View Requests</button></a>
   </body>
