616a3f1956f4777d5405f01187973d9fe4e0a55f
tchen <tchenGIT@users.noreply.github.com>
2015-11-01 17:14:05 -0800

    added check for non-existent course list

--- student/course.php
+++ student/course.php
@@ -9,20 +9,26 @@
 
 <h2 class = "header">SCUWaitlister</h2>
 
-<h4 class = "des">Select Course</h4>
+<h4 class = "des">Select Course & Section</h4>
+
+<?php
+  require('../fcn/parseCourseList.php');
+  $filename = '../resource/' . $_POST['department'] . '/courseList.csv';
+  if( ! file_exists($filename))
+    die("The ". $_POST['department'] ." Department has not uploaded any course information.");
+?>
 
 <p>
   Select the Section of the Course you wish to request for waitlisting.<br />
   The listing format is "CourseNumber [SectionNumber] (CourseName)".</br></br>
-  A full list of classes is available at <a href="http://www.scu.edu/courseavail/" target="_blank">CourseAvail.</a></p>
+  A full list of classes is available at <a href="http://www.scu.edu/courseavail/" target="_blank">CourseAvail.</a>
+</p>
 
 	<form action="waitlist.php" method = "post">
         Course: <?php echo $_POST['department'];?>
 		<select required id = "course" name = "course">
-                <option value = "">Select one</option>
+      <option value = "">Select one</option>
 			<?php 
-			require('../fcn/parseCourseList.php');
-			$filename = '../resource/' . $_POST['department'] . '/courseList.csv';
 			//open course list
 			$courses = array();
 			$courses = parseCourseList($filename);
@@ -30,10 +36,9 @@
 			foreach ($courses as $key=>$value)
        echo "<option value='$key'>$value[0] [$key] ($value[1])</option>";
 			?>
-		
-        </select>
-        <button type = "submit" value = "Submit">Submit</button>
-        </br></br>
+    </select>
+    <button type = "submit" value = "Submit">Submit</button>
+    </br></br>
 
 
 </body>
