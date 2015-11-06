964636a11b1e49b7ba4561c34f9f4d9b17d1124c
tchen <tchenGIT@users.noreply.github.com>
2015-11-06 13:48:55 -0800

    updated admin to work with getDepartment function

--- admin/index.php
+++ admin/index.php
@@ -2,8 +2,12 @@
 <html>
   <body>
     <h1>SCUWaitLister Admin Page</h1>
+    <?php
+      require "../fcn/getDepartment.php";
+      $theDepartment = getDepartment();
+    ?>
     <p>
-      You are using this system as the <?php echo $_SERVER['REMOTE_USER']; ?> Department.<br />
+      You are using this system as the <?php echo $theDepartment; ?> Department.<br />
       To change the Department, please restart the browser.
     </p>
     <a href='../index.html'><button>Return</button></a>
