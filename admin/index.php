71cf2acb937991d886013435dfc729099b5366da
sanzstv <ssanz@scu.edu>
2015-11-09 15:56:38 -0800

    admin styling

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title><?php echo $theDepartment; ?> Administrator Page| SCUWaitLister</title>
+		<title>Administrator Page| SCUWaitLister</title>
 		
 		<link rel = "stylesheet" type = "text/css" href = "../css/style.css">
 	</head>
@@ -19,15 +19,10 @@
       You are using this system as the <?php echo $theDepartment; ?> Department.<br />
       To change the Department, please restart the browser.
     </p>
-<<<<<<< HEAD
     <a class = "return" href='../index.html'>Return</a>
     <a class = "submit" href='upload.php'>Update Course</a>
     <a class = "submit" href='select_waitlist.php'>View Requests</a>
-=======
-    <a class = "return" href='../index.html'><button>Return</button></a>
-    <a class = "submit" href='upload.php'><button>Update Course</button></a>
-    <a class = "submit" href='select_waitlist.php'><button>View Requests</button></a>
->>>>>>> eda462fcbae3fa1313480aacbf581a240d29070a
+    
 </div>
   </body>
 </html>
